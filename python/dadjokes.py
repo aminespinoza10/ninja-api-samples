274f555b57c40fc4f5f9f99dfb9fc272ae5bb031
aminespinoza10 <amin.espinoza@outlook.com>
2023-08-05 12:17:15 -0600

    env file sample for python

--- python/dadjokes.py
+++ python/dadjokes.py
@@ -1,8 +1,15 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
 
 limit = 3
 api_url = 'https://api.api-ninjas.com/v1/dadjokes?limit={}'.format(limit)
-response = requests.get(api_url, headers={'X-Api-Key': 'M/Gc0kc1pWefKueDDnw+Xg==wQSdncZeeouQVrBj'})
+response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
 if response.status_code == requests.codes.ok:
     print(response.text)
 else:
